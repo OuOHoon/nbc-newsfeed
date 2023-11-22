cbfe3fbe07e6dfa763e5c05d9d2ac37a1dfaedc1
Dongha <wkdehdgk159@gmail.com>
2023-11-22 17:55:13 +0900

    Fix(Signup regexp): 정규표현식 오타가 있어 수정했습니다~

--- src/main/java/com/sparta/newsfeed/user/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/newsfeed/user/dto/SignupRequestDto.java
@@ -15,7 +15,7 @@ public class SignupRequestDto {
     private String username;
 
     @NotBlank
-    @Pattern(regexp = "^[A-Za-z0-9]{8,15}+$")
+    @Pattern(regexp = "^[A-Za-z0-9]{8,15}$")
     //최소 8자 이상, 15자 이하이며 알파벳 대소문자(a~z, A~Z), 숫자(0~9)
     private String password;
 
