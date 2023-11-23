92f9416f77c51f7e7da7daea3e77ed8045f1de4c
Dongha <wkdehdgk159@gmail.com>
2023-11-23 14:38:32 +0900

    Refactor(Remove Unnecessary Try-catch): GlobalExceptionController에서 잡아주는 불필요한 try-catch문 삭제했습니다.

--- src/main/java/com/sparta/newsfeed/user/UserController.java
+++ src/main/java/com/sparta/newsfeed/user/UserController.java
@@ -27,28 +27,16 @@ public class UserController {
     //회원가입
     @PostMapping("/signup")
     public ResponseEntity<BaseResponse<SignupResponseDto>> signup(@Valid @RequestBody SignupRequestDto requestDto) {
-        try {
-            userService.signup(requestDto);
-        } catch (ExistingUserException e) {
-            //존재하는 아이디일 시 오류메시지 반환
-            return ResponseEntity.badRequest().body(BaseResponse.of(e.getMessage(), HttpStatus.BAD_REQUEST.value(), null));
-        }
+        userService.signup(requestDto);
         return ResponseEntity.ok().body(BaseResponse.of("회원가입 성공", HttpStatus.CREATED.value(), new SignupResponseDto(requestDto.getUsername())));
     }
 
     //로그인
     @PostMapping("/login")
     public ResponseEntity<BaseResponse<Void>> login(@RequestBody LoginRequestDto requestDto, HttpServletResponse response) {
-        try {
-            userService.login(requestDto);
-        } catch (NotFoundUserException | WrongPasswordException e) {
-            //DB에 없는 아이디거나 비밀번호 오류 시 오류메시지 반환
-            return ResponseEntity.badRequest().body(BaseResponse.of(e.getMessage(), HttpStatus.BAD_REQUEST.value(), null));
-        }
-
-        //로그인 성공 시 토큰 생성 후 헤더에 넣기
+        userService.login(requestDto);
+        //로그인 가능 시 토큰 생성 후 헤더에 넣기
         response.setHeader("Authorization", jwtUtil.createToken(requestDto.getUsername(), true));
-
         return ResponseEntity.ok().body(BaseResponse.of("로그인 성공", HttpStatus.OK.value(), null));
     }
 
@@ -57,19 +45,12 @@ public class UserController {
     public ResponseEntity<BaseResponse<Void>> logout(HttpServletResponse response, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         //만료기한이 0 인 토큰 발급
         response.setHeader("Authorization", jwtUtil.createToken(null, false));
-
         return ResponseEntity.ok().body(BaseResponse.of("로그아웃 성공", HttpStatus.OK.value(), null));
     }
 
     @PutMapping("/password")
     public ResponseEntity<BaseResponse<Void>> changePassword(@Valid @RequestBody ChangePasswordRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        try {
-            userService.changePassword(requestDto, userDetails);
-        } catch (WrongPasswordException | SamePasswordException e) {
-            //비밀번호 틀렸거나, 기존의 패스워드와 바꾸려는 패스워드가 같을 때
-            return ResponseEntity.badRequest().body(BaseResponse.of(e.getMessage(), HttpStatus.BAD_REQUEST.value(), null));
-        }
-
+        userService.changePassword(requestDto, userDetails);
         return ResponseEntity.ok().body(BaseResponse.of("패스워드 변경 성공!", HttpStatus.OK.value(), null));
     }
 }
