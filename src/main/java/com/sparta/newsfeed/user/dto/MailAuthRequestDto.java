058c3c4601b6125ed5b19ee8fdeeedd304eb6c6a
Dongha <wkdehdgk159@gmail.com>
2023-11-26 16:50:13 +0900

    Refactor(userAuth): 인증코드 양식 명확히

--- src/main/java/com/sparta/newsfeed/user/dto/MailAuthRequestDto.java
+++ src/main/java/com/sparta/newsfeed/user/dto/MailAuthRequestDto.java
@@ -2,6 +2,8 @@ package com.sparta.newsfeed.user.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,8 +11,7 @@ import lombok.Setter;
 @Setter
 public class MailAuthRequestDto {
 
-    @NotBlank
-    @NotNull
+    @Pattern(regexp = "/[0-9]{6}$/", message = "인증번호는 6자리 숫자입니다.")
     private int authcode;
 
 }
